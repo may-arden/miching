d298e47b6a7df86dd951c65c7d6353edc461d0a3
may arden <mayard3n@gmail.com>
2020-11-18 14:26:35 -0800

    pushing

---
+++ environment.rb
@@ -0,0 +1,8 @@
+require "bundler"
+Bundler.require 
+
+require_relative "./lib/miching/version"
+require_relative './lib/miching/cli'
+require_relative './lib/miching/api'
+require_relative './lib/miching/song'
+
